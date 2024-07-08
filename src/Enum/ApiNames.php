06effd6ba1bf4876efaf4cdc3fcd59b7e3ce5e31
Egor Ermilov <egor.ermilov1992@gmail.com>
2024-07-08 13:18:15 +0300

    Добавление перечисления имен взаимодействия

---
+++ src/Enum/ApiNames.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MgermApiClasses\Enum;
+
+/**
+ * Перечисление для имен сервисов взаимодействия
+ */
+
+final class ApiNames
+{
+    public const Ivoice = 'ivoice';
+    public const ProDoctorov = 'prodoctorov';
+    public const Wazapa = 'wazapa';
+    public const EVostok = 'evostok';
+    public const MangoTelecom = 'mangotelecom';
+}
