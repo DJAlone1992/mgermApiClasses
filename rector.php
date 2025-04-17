f1bb94ef6c0b0f61958496bf72bf846693c1ba23
Egor Ermilov <egor.ermilov1992@gmail.com>
2025-04-17 12:04:07 +0300

    rector

---
+++ rector.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\ValueObject\PhpVersion;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    // uncomment to reach your current PHP version
+    ->withPhpSets()
+    ->withDeadCodeLevel(0)
+    ->withTypeCoverageLevel(100000);
