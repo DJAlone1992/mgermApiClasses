9970f9727c1a0fe0b0370cdd307965b1a09d8432
Egor Ermilov <egor.ermilov1992@gmail.com>
2026-07-07 12:25:24 +0300

    rector

--- src/Classes/DrugStore/ContractTable.php
+++ src/Classes/DrugStore/ContractTable.php
@@ -208,12 +208,18 @@ class ContractTable extends BaseClass
     {
         return $this->differenceTable;
     }
-    public function setDifferenceTable(array $differenceTable): static
+    /**
+     * @return static
+     */
+    public function setDifferenceTable(array $differenceTable)
     {
         $this->differenceTable = $differenceTable;
         return $this;
     }
-    public function addDifferenceDrug(int $drugID, int $countDifference, int $sumDifference): static
+    /**
+     * @return static
+     */
+    public function addDifferenceDrug(int $drugID, int $countDifference, int $sumDifference)
     {
         $this->differenceTable[$drugID] = [
             'count' => $countDifference,
